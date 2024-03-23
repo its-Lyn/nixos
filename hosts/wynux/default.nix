ad37608ee472f9e0f87d6dc8b906ca8c43e2806e
its-Lyn <143884194+its-Lyn@users.noreply.github.com>
2024-03-23 21:50:14 +0200

    Switch to gnome.

--- hosts/wynux/default.nix
+++ hosts/wynux/default.nix
@@ -43,9 +43,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
